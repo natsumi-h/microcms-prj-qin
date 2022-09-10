6314e3d9bdfa73436c008290964f9e41ddf137a5
Natsumi.H <natsmy.1211@gmail.com>
2022-09-10 20:52:50 +0800

    test branch

--- components/layout/top/twitter/twitter.jsx
+++ components/layout/top/twitter/twitter.jsx
@@ -4,9 +4,9 @@ export function Twitter() {
   return (
     <div>
       <ul>
-        <li>twitterlist</li>
-        <li>twitterlist</li>
-        <li>twitterlist</li>
+        <li>twitterlist!</li>
+        <li>twitterlist!</li>
+        <li>twitterlist!</li>
       </ul>
       <Button text="View on Twitter" href="https://twitter.com/" />
     </div>
